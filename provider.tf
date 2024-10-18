22eb7eae83bb63557efb9720b62afac35476e401
Aquabet <jiaxianli57@gmail.com>
2024-10-18 08:32:13 -0700

    fmt provider.tf vpc.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   profile = var.aws_profile
-  region = var.aws_regions[0]
+  region  = var.aws_regions[0]
 }
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "csye6225" {
-  count = length(var.aws_regions)
+  count                = length(var.aws_regions)
   cidr_block           = var.vpc_cidrs[count.index]
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -9,7 +9,7 @@ resource "aws_vpc" "csye6225" {
 }
 
 resource "aws_internet_gateway" "gw" {
-  count =  length(var.aws_regions)
+  count  = length(var.aws_regions)
   vpc_id = aws_vpc.csye6225[count.index].id
   tags = {
     Name = "csye6225_gateway"
@@ -17,7 +17,7 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "aws_route_table" "public_rt" {
-  count = length(var.aws_regions)
+  count  = length(var.aws_regions)
   vpc_id = aws_vpc.csye6225[count.index].id
   route {
     cidr_block = var.public_cidr_block
@@ -29,7 +29,7 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route_table" "private_rt" {
-  count = length(var.aws_regions)
+  count  = length(var.aws_regions)
   vpc_id = aws_vpc.csye6225[count.index].id
   tags = {
     Name = "private-route-table"
