9ed2d90eee9d749a1cd7a7c8ee578f7359cbb30f
Aquabet <jiaxianli57@gmail.com>
2024-10-18 08:29:12 -0700

    new feature: now we can set regoins

--- subnets.tf
+++ subnets.tf
@@ -1,32 +1,32 @@
 resource "aws_subnet" "csye6225_public" {
-  count                   = 3
-  vpc_id                  = aws_vpc.csye6225.id
-  cidr_block              = var.public_subnet_cidrs[count.index]
+  count                   = 3 * length(var.aws_regions)
+  vpc_id                  = aws_vpc.csye6225[floor(count.index / 3)].id
+  cidr_block              = var.public_subnet_cidrs[count.index % 3]
   map_public_ip_on_launch = true
-  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index % 3)
   tags = {
-    Name = "public_subnet_${count.index + 1}"
+    Name = "public_subnet_${count.index}"
   }
 }
 
 resource "aws_subnet" "csye6225_private" {
-  count             = 3
-  vpc_id            = aws_vpc.csye6225.id
-  cidr_block        = var.private_subnet_cidrs[count.index]
-  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  count             = 3 * length(var.aws_regions)
+  vpc_id            = aws_vpc.csye6225[floor(count.index / 3)].id
+  cidr_block        = var.private_subnet_cidrs[count.index % 3]
+  availability_zone = element(data.aws_availability_zones.available.names, count.index % 3)
   tags = {
-    Name = "private_subnet_${count.index + 1}"
+    Name = "private_subnet_${count.index}"
   }
 }
 
 resource "aws_route_table_association" "csye6225_public_assoc" {
-  count          = length(var.public_subnet_cidrs)
+  count          = 3 * length(var.aws_regions)
   subnet_id      = aws_subnet.csye6225_public[count.index].id
-  route_table_id = aws_route_table.public_rt.id
+  route_table_id = aws_route_table.public_rt[floor(count.index / 3)].id
 }
 
 resource "aws_route_table_association" "csye6225_private_assoc" {
-  count          = length(var.private_subnet_cidrs)
+  count          = 3 * length(var.aws_regions)
   subnet_id      = aws_subnet.csye6225_private[count.index].id
-  route_table_id = aws_route_table.private_rt.id
+  route_table_id = aws_route_table.private_rt[floor(count.index / 3)].id
 }
